839ff9442a1c4b0a68bb093917da9e21cd0a0b72
MakDIGer <makarin@kgdnet.ru>
2016-04-26 15:54:05 +0200

    Added four classes

---
+++ app/Core.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Класс ядро вебсайта.
+ * User: nikolay
+ * Date: 26.04.16
+ * Time: 15:48
+ */
+
+namespace app;
+
+
+class Core
+{
+    public $p;
+    public $pp;
+
+    public function router($p, $pp)
+    {
+        //TODO написать функцию роутера;
+    }
+}
--- web/index.php
+++ web/index.php
@@ -8,6 +8,9 @@
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../app/DataBase.php';
+
+$DB_class = new \app\DataBase('localhost', 'root', 'mak2526669', 'guest_book');
 
 $loader = new Twig_Loader_Filesystem(__DIR__ . '/../view');
 $twig = new Twig_Environment($loader, array(
